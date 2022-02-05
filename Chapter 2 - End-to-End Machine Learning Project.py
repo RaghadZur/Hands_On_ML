2e15a26677f957f8371a2dce8c32842d514d5e57
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-02-05 14:45:09 +0000

    splitting the data

--- Chapter 2 - End-to-End Machine Learning Project.py
+++ Chapter 2 - End-to-End Machine Learning Project.py
@@ -37,3 +37,30 @@ housing = load_housing_data()
 
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% DISCOVERING THE DATA %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 print(housing.head())
+
+print(housing.info())
+
+print(housing.describe())
+
+import matplotlib.pyplot as plt
+
+# Plotting a histogram for the numerical attributes
+housing.hist(bins=50, figsize=(20, 15))
+plt.show()
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% SPLITTING THE DATASET %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+import numpy as np
+
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+
+train_set, test_set = split_train_test(housing, 0.2)
+print(len(train_set))
+print(len(test_set))
