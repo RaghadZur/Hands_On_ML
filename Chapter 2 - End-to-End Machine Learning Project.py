bda854617f5b4b599b3a34dec2206dd1a106aa69
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-02-06 22:01:34 +0000

    splitting the data

--- Chapter 2 - End-to-End Machine Learning Project.py
+++ Chapter 2 - End-to-End Machine Learning Project.py
@@ -65,7 +65,20 @@ def split_train_test(data, test_ratio):
     return data.iloc[train_indices], data.iloc[test_indices]
 
 
-# calling the split functino
+# calling the split function
+
 train_set, test_set = split_train_test(housing, 0.2)
 print(len(train_set))
 print(len(test_set))
+
+from zlib import crc32
+
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2 ** 32
+
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+    return data.loc[~in_test_set], data.loc[in_test_set]
