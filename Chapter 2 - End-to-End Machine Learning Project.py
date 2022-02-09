adfff137354b92cd2ab2b529b528088ca7c35d0e
Raghad <72172564+RaghadZur@users.noreply.github.com>
2022-02-09 20:54:37 +0000

    splitting the data

--- Chapter 2 - End-to-End Machine Learning Project.py
+++ Chapter 2 - End-to-End Machine Learning Project.py
@@ -80,3 +80,9 @@ def split_train_test_by_id(data, test_ratio, id_column):
     ids = data[id_column]
     in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
     return data.loc[~in_test_set], data.loc[in_test_set]
+
+
+import hashlib
+
+def test_set_check(identifier, test_ratio, hash=hashlib.md5):
+    return hash(np.int64(identifier)).digest()[-1] < 256 * test_ratio
